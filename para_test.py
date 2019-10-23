c14c6c2d278df52cf52c99d1e947980ac7c3ad00
dancrew32 <dancrew32@gmail.com>
2019-10-23 00:08:07 -0700

    callbacks and better tests

--- para_test.py
+++ para_test.py
@@ -1,19 +1,28 @@
 import unittest
+from unittest import mock
 
 import para
 
 
 def handler(value):
-    print(value)
+    return f'{value}_yay'
+
+
+def callback(result):
+    print(result)
 
 
 class TestPara(unittest.TestCase):
 
-    def test_by_thread(self):
-        para.by_thread(handler, ['one', 'two'], 2)
+    @mock.patch('__main__.callback')
+    def test_by_thread(self, cb):
+        para.by_thread(handler, ['one', 'two'], callback, workers=2)
+        self.assertEqual(2, cb.call_count)
 
-    def test_by_process(self):
-        para.by_process(handler, ['one', 'two'], 2)
+    @mock.patch('__main__.callback')
+    def test_by_process(self, cb):
+        para.by_process(handler, ['one', 'two', 'three'], callback, workers=2)
+        self.assertEqual(3, cb.call_count)
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -24,13 +24,18 @@ def handler(url):
     return re.findall(r'<title>(.*?)</title>', data)
 
 
+def callback(titles):
+    print(titles)
+
+
 urls = (
     'https://danmasq.com', 
     'https://www.google.com', 
     'http://news.ycombinator.com', 
-    'http://reddit.com'
+    'http://reddit.com',
+    'http://ebay.com',
 )
 
-para.by_thread(handler, urls, workers=5)
-para.by_process(handler, urls, workers=5)
+para.by_thread(handler, urls, callback, workers=5)
+para.by_process(handler, urls, callback, workers=5)
 ```
